b561068bf06aeaa79504a910c38e0f456ebf4d7f
Felipão <feliperodvrt@gmail.com>
2024-09-04 21:03:13 -0300

    04/09

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
     
+    var nr_c = "";
+
+    $('.bt-calc').click(function() {
+    
+        if (nr_c.length < 25) {
+            var nr = $(this).text();
+
+            nr_c = nr_c.concat(nr);
+
+            $('#txt_display').text(nr_c);
+        }
+    });
+
+    var nr_up = "";
+
+    $('#bt-plus').click(function(){
+
+        nr_up = nr_c
+        nr_c = ""
+        $('#txt_display').text("0");
+        $('#txt_operadores').text(nr_up);
+        
+        })
+
+
+    $('#bt-equal').click(function() {
+        var result = nr_up + nr_c
+        result = result.replace("=", "")
+        result = eval(result)
+        
+
+    })
 });
+
+
+
 
     
