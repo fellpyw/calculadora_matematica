9d9bed63cc0aab09843da16982dfb2a24398f6c2
fellpyw <feliperodvrt@gmail.com>
2024-09-11 20:49:12 -0300

    aula 11/09 portiolli simulator
    11 de setembro; Torres Gêmeas; Ataque Terrorista; Avião bater em prédio;

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,13 @@
 $(document).ready(function() {
     
-    var nr_c = "";
-
     $('.bt-calc').click(function() {
-    
-        if (nr_c.length < 25) {
-            var nr = $(this).text();
+        var bt_nr = $(this).text();
 
-            nr_c = nr_c.concat(nr);
+        $('#txt_display').val(bt_nr);
 
-            $('#txt_display').text(nr_c);
-        }
-    });
-
-    var nr_up = "";
-
-    $('#bt-plus').click(function(){
-
-        nr_up = nr_c
-        nr_c = ""
-        $('#txt_display').text("0");
-        $('#txt_operadores').text(nr_up);
-        
-        })
-
-
-    $('#bt-equal').click(function() {
-
-        var result = nr_up + nr_c
-        result = result.replace("=", "")
-        result = eval(result)
-        $('#txt_display').text(result);
-    
-    })
-
-
+    })     
 });
+
 
 
 
